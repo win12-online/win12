10bb2e6e418f53b6e412eb48b84d716727a5eb44
oliver <oliverxiaozhen@outlook.com>
2026-05-21 22:00:38 +0800

    tauri:使用Vite打包

--- tauri/vite.config.ts
+++ tauri/vite.config.ts
@@ -1,30 +1,14 @@
 import { defineConfig } from "vite";
 
-// @ts-expect-error process is a nodejs global
-const host = process.env.TAURI_DEV_HOST;
-
-// https://vite.dev/config/
-export default defineConfig(async () => ({
-
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent Vite from obscuring rust errors
-  clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
+export default defineConfig({
+  root: "..",
+  base: "./",
   server: {
-    port: 1420,
+    port: 5173,
     strictPort: true,
-    host: host || false,
-    hmr: host
-      ? {
-          protocol: "ws",
-          host,
-          port: 1421,
-        }
-      : undefined,
-    watch: {
-      // 3. tell Vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
-    },
   },
-}));
+  build: {
+    outDir: "tauri/dist",
+    emptyOutDir: true,
+  },
+});
